8aad3d32d443bea58b084e73e75fcd32f701217c
Rohit Kumar Singh <55323686+voidrohit@users.noreply.github.com>
2024-04-07 12:26:51 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,39 @@
+import { Config } from "tailwindcss";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+
+// import defaultTheme from "tailwindcss/defaultTheme";
+// import colors from "tailwindcss/colors";
+// import { flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [
+    addVariablesForColors,
+  ],
+};
+export default config;
