5d3c70621e894988dae0221cfc838966ccd2ca37
Rohit Kumar Singh <55323686+voidrohit@users.noreply.github.com>
2024-04-07 12:48:23 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,51 +2,13 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
+import { projects } from "./projects";
 
 import Navbar from "@/components/ui/navbar"
 import { Bigtilt } from "@/components/ui/bigtilt";
 import { WavyBackground } from "@/components/ui/wavy-background";  
 
 import { HoverEffect } from "../components/ui/card-hover-effect";
-
-export const projects = [
-  {
-    title: "Media Production and Post-Production",
-    description:
-      "Transferring large video files, raw footage, and other multimedia content between production studios, post-production facilities, and distribution networks, enabling efficient collaboration and meeting tight deadlines",
-    link: "/media-production-and-post-production",
-  },
-  {
-    title: "Genomic Research and Bioinformatics",
-    description:
-      "Sharing and distributing massive genomic datasets, such as whole-genome sequences and molecular data, across research institutions and computing centers for analysis, enabling collaborative research and discoveries.",
-    link: "/genomic-research-and-bioinformatics",
-  },
-  {
-    title: "Cloud Migration and Disaster Recovery",
-    description:
-      "Migrating large amounts of data between on-premises infrastructure and cloud environments, or replicating data across multiple cloud regions for redundancy and disaster recovery purposes, ensuring business continuity and data protection",
-    link: "/cloud-migration-and-disaster-recovery",
-  },
-  {
-    title: "In-Cloud Migration",
-    description:
-      "Migrating large datasets, applications, and workloads from on-premises infrastructure to cloud environments or between different cloud providers.",
-    link: "/in-cloud-migration",
-  },
-  {
-    title: "File Migration",
-    description:
-      "Migrating data between two sites, into public cloud storage, or for storage consolidation, and exponentially shortens transfer times with its record-setting efficiency and incremental sync for convergence.",
-    link: "/file-migration",
-  },
-  {
-    title: "Hybrid Cloud Acceleration",
-    description:
-      "Data moves not fast enough on-prem, across facilities, or between private and public clouds, in either file or object form? Zetarya efficiency and performance come in handy.",
-    link: "",
-  },
-];
 
 const World = dynamic(() => import("../components/ui/globe").then((m) => m.World), {
   ssr: false,
