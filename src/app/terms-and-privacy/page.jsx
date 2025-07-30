a4bdeacf16830a8df136c6ba770dab890e9ecef8
rohit19872 <rohitkumar.singh@eka.care>
2025-07-30 23:41:07 +0530

    Updated Privacy Policy

--- src/app/terms-and-privacy/page.jsx
+++ src/app/terms-and-privacy/page.jsx
@@ -62,7 +62,7 @@ const PrivacyPolicy = () => {
             </p>
         </section>
         <section className="mb-12">
-            <h2 className="text-xl font-semibold mb-4">Pricing disclaimer</h2>
+            <h2 className="text-2xl font-bold mb-4">Pricing disclaimer</h2>
             <p className='text-gray-700 mb-4'>
             Prices indicated on this website are subject to change without prior notice from zero2. Request an official quote for the purpose of purchase processing. No refunds will be issued after the purchase of Zetarya services..
             </p>
@@ -77,12 +77,63 @@ const PrivacyPolicy = () => {
             </p>
         </section>
         <section className="mb-12">
-            <h2 className="text-2xl font-bold mb-4">Intro</h2>
+            <h1 className="text-4xl font-bold mb-4">Intro</h1>
             <p className="text-gray-700 mb-4">
             Our Privacy Policy covers processing of all personal data that we may collect anytime you visit our website or purchase our products and services. “We” shall mean the companies, identified in Contact us section below.
             </p>
-        </section>
+            </section>
+            <h1 className="text-4xl font-bold mb-8">Privacy Statement</h1>
+            <section className="mb-12">
+                <h2 className="text-2xl font-bold mb-4">What we collect</h2>
+                <ul className="custom-list">
+                    <li className='text-gray-700 mb-4'>Full name</li>
+                    <li className='text-gray-700 mb-4'>Email</li>
+                    <li className='text-gray-700 mb-4'>The size of any files or data you send</li>
+                    <li className='text-gray-700 mb-4'>Recipient email address</li>
+                    <li className='text-gray-700 mb-4'>General information includes browser type, geo location, os type.</li>
+                </ul>
+            </section>
+            <h1 className="text-4xl font-bold mb-8">How we use the data</h1>
+            <section className="mb-12">
+                <p className="text-gray-700 mb-4">We use general information about you to improve performance and usability of our website and desktop application.</p>
+                <p className="text-gray-700 mb-4">We use your personal data to contact you and provide you with:</p>
+                <ul className="custom-list">
+                    <li className='text-gray-700 mb-4'>Our product and services, for example, send you the licenses and provide you with support.</li>
+                    <li className='text-gray-700 mb-4'>Information about the product that you evaluate or purchase. Here are examples for reasons to send you notifications: updates on the change the product functionality, changes to supported platforms, inclusion of a new technology, product or feature discontinuation, legal notices.</li>
+                    <li className='text-gray-700 mb-4'>Support renewal reminders.</li>
+                </ul>
+                <p className='text-gray-700 mb-4'>With your permission we may send you information on the new releases that significantly enhance the product functionality, or more advanced products out of our product line. We may also send you offers with privileged prices. You can let us know anytime if you do not want to receive this information any more. See the Contact us section of this policy.</p>
+            </section>
         </div>
+        <footer className="bg-[#171515] text-[#F5F5F7] py-8">
+            <div className="flex flex-row justify-between h-[200px] max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="">
+                    <h2 className="mb-4">CONTACT US</h2>
+                    <p className="text-base lg:text-5xl mb-4 font-semibold">info@zetarya.com</p>
+                </div>
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-2 gap-4">
+                    <div>
+                        <a href="/docs" className="text-sm hover:underline">Docs</a>
+                    </div>
+                    <div>
+                        <a href="/terms-and-privacy" className="text-sm hover:underline">Term & Privacy</a>
+                    </div>
+                    <div>
+                        <a href="/blogs" className="text-sm hover:underline">Blogs</a>
+                    </div>
+                    {/*<div>*/}
+                    {/*    <a href="/pricing" className="text-sm hover:underline">Pricing</a>*/}
+                    {/*</div>*/}
+                    <div>
+                        <a href="https://www.zero2.in" className="text-sm hover:underline">Company</a>
+                    </div>
+                    <div>
+                        <a href="/contact" className="text-sm hover:underline">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <p className="text-gray-700 bg-[#171515] w-full text-center pb-2">© zero2 All rights reserved</p>
     </div>
   );
 };
