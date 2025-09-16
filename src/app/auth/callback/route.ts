f57878855dbe426f9637291968d7d70dbdadc7f1
Rohit Kumar Singh <55323686+voidrohit@users.noreply.github.com>
2025-09-16 15:35:23 +0530

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -7,15 +7,16 @@ export async function GET(request: any) {
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/'
-
+  console.log(origin, searchParams)
   if (code) {
     const supabase = await createClientForServer()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
         return NextResponse.redirect(`${origin}/dashboard`)
     }
+    console.log(error)
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+  // return NextResponse.redirect(`${origin}/auth/auth-code-error`)
 }
